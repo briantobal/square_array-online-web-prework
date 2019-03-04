549fe97c481f03f24ecaed3aac3d60ec66c650fd
Brian Tobal <brian.tobal@gmail.com>
2019-03-04 22:42:34 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,7 @@
+
+
 def square_array(array)
-  count = 0 
-  while count < array.size 
-    array[count]**2
-    count += 1
+  new_array = []
+  array.each{|a| new_array.push(a*a)}
+  return new_array
 end
